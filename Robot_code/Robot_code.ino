49e3fecffcde88536da4d0fce135177593d38c9d
Ciolek <e448193@us.lmco.com>
2023-07-31 11:22:12 -0400

    Changed from one speed to a low and high speed for curves vs straight path Set up ultrasonic sensors, but still need to add autonomous driving aspect

--- Robot_code/Robot_code.ino
+++ Robot_code/Robot_code.ino
@@ -1,7 +1,10 @@
 #include "MeMegaPi.h"
+
+// Create Object Names
 MeLineFollower lineFinder;
 MeRGBLed LED1, LED2;
 MeUltrasonicSensor US1, US2, US3;
+MeLimitSwitch LS1, LS2;
 
 // Initialize motors
 MeMegaPiDCMotor motor1(PORT1A);
@@ -9,68 +12,90 @@ MeMegaPiDCMotor motor2(PORT1B);
 MeMegaPiDCMotor motor3(PORT2A);
 MeMegaPiDCMotor motor4(PORT2B);
 
-// Set motor speed
-uint8_t motorSpeed = 100;
+// Set motor speeds
+uint8_t highSpeed = 100;
+uint8_t lowSpeed = 50;
+
+//Mode 0 = Line Tracing
+//Mode 1 = Extraction Zone/Obstacle Detection
+//Mode 2 = Gesture Recognition
+uint8_t mode = 0;
+
+//Counter for Impact Switch
+uint8_t impactCount = 0;
 
 void setup() {
-  //Set Initial Pins
-  lineFinder.setpin(A9,A10);
-  LED1.setpin(A13);
-  LED2.setpin(A14);
+  //Define Pins of Objects
   US1.setpin(A6);
   US2.setpin(A7);
   US3.setpin(A8);
-
-  //Mode 0 = Line Tracing
-  //Mode 1 = Obstacle Detection
-  //Mode 2 = Gesture Recognition
-  int mode = 0;
-  //Counter for Impact Switch
-  int count = 0;
+  lineFinder.setpin(A9, A10);
+  LS1.setpin(A11);
+  LS2.setpin(A12);
+  LED1.setpin(A13);
+  LED2.setpin(A14);
 }
 
 void loop() {
-
-  switch(mode) {
-
-  case 0:
   // Read the state of the line sensors
-  int ltsensorState = lineFinder.readSensors();
+  int ltSensorState = lineFinder.readSensors();
 
-  //Set Initial LED Colors and Show them
-  LED1.setColor(0,255,255,255);
-  LED2.setColor(0,255,255,255);
-  // LED1.show();
-  // LED2.show();
-
-  // If both sensors are over the line, move forward
-  if(ltsensorState == S1_IN_S2_IN) {
-    motor1.run(motorSpeed);
-    motor2.run(motorSpeed);
-    motor3.run(-motorSpeed);
-    motor4.run(-motorSpeed);
-  }
-  // If the left sensor is over the line, turn left
-  else if(ltsensorState == S1_IN_S2_OUT) {
-    motor1.run(motorSpeed);
-    motor2.run(motorSpeed);
-    motor3.run(motorSpeed);
-    motor4.run(motorSpeed);
-  }
-  // If the right sensor is over the line, turn right
-  else if(ltsensorState == S1_OUT_S2_IN) {
-    motor1.run(-motorSpeed);
-    motor2.run(-motorSpeed);
-    motor3.run(-motorSpeed);
-    motor4.run(-motorSpeed);
-  }
-  // If neither sensor is over the line, stop
-  else {
-    motor1.stop();
-    motor2.stop();
-    motor3.stop();
-    motor4.stop();
+  if (LS1.touched() && LS2.touched()) {
+    impactCount++;
+    if (impactCount == 3) {
+      if (mode == 0 && ltSensorState == S1_OUT_S2_OUT) {
+        mode = 1;
+      }
+      impactCount = 0;
+    }
   }
 
+  switch (mode) {
+    case 0:
+      //Set Initial LED Colors and Show them
+      LED1.setColor(0, 255, 255, 255);
+      LED2.setColor(0, 255, 255, 255);
+      // LED1.show();
+      // LED2.show();
+
+      // If both sensors are over the line, move forward
+      if (ltSensorState == S1_IN_S2_IN) {
+        motor1.run(highSpeed);
+        motor2.run(highSpeed);
+        motor3.run(-highSpeed);
+        motor4.run(-highSpeed);
+      }
+      // If the left sensor is over the line, turn left
+      else if (ltSensorState == S1_IN_S2_OUT) {
+        motor1.run(lowSpeed);
+        motor2.run(lowSpeed);
+        motor3.run(lowSpeed);
+        motor4.run(lowSpeed);
+      }
+      // If the right sensor is over the line, turn right
+      else if (ltSensorState == S1_OUT_S2_IN) {
+        motor1.run(-lowSpeed);
+        motor2.run(-lowSpeed);
+        motor3.run(-lowSpeed);
+        motor4.run(-lowSpeed);
+      }
+      // If neither sensor is over the line, stop
+      else {
+        motor1.stop();
+        motor2.stop();
+        motor3.stop();
+        motor4.stop();
+      }
+      break;
+
+    case 1:
+    //Still need to add driving aspect
+      if (US1.distanceCm(400)<=10 || US2.distanceCm(400)<=10 || US3.distanceCm(400)<=10) {
+        motor1.stop();
+        motor2.stop();
+        motor3.stop();
+        motor4.stop();
+        delay(750);
+      }
   }
 }
